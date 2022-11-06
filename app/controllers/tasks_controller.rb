a84c3f1fccb77db6c67434cefc8501ca192b382a
井出晶 <badminton.1110@i.softbank.jp>
2022-11-06 12:47:19 +0900

    ステップ1完成

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
-    before_action :set_task, only: [ :show, :edit, :update, :destroy ] 
-  
+  before_action :set_task, only: %i[ show edit update destroy ]
+
   def index
     @tasks = Task.all
   end
@@ -11,10 +11,8 @@ class TasksController < ApplicationController
 
   def create
     @task = Task.new(task_params)
-    #@task = current_user.tasks.build(task_params)
     if @task.save
-      redirect_to tasks_path
-      flash[:notice] = t('.created')
+      redirect_to tasks_path, notice: 'Task was successfully created'
     else
       render :new
     end
@@ -26,17 +24,24 @@ class TasksController < ApplicationController
   def edit
   end
 
- 
+  def update
+    if @task.update(task_params)
+      redirect_to tasks_path, notice: 'Task was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
   def destroy
     @task.destroy
-    redirect_to tasks_path, notice: t('.destroyed')
+    redirect_to tasks_path, notice: 'Task was successfully destroyed.'
   end
 
   private
 
-    #def set_task
-      #@task = Task.find(params[:id])
-    #end
+    def set_task
+      @task = Task.find(params[:id])
+    end
 
     def task_params
       params.require(:task).permit(:title, :content)
