62b63dc3eff376878170d1224010720eaa9e91b3
井出晶 <badminton.1110@i.softbank.jp>
2022-11-09 18:52:43 +0900

    flash導入

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,7 +12,8 @@ class TasksController < ApplicationController
   def create
     @task = Task.new(task_params)
     if @task.save
-      redirect_to tasks_path, notice: 'Task was successfully created'
+      redirect_to tasks_path
+      flash[:notice] = "Task was successfully created."
     else
       render :new
     end
@@ -27,6 +28,7 @@ class TasksController < ApplicationController
   def update
     if @task.update(task_params)
       redirect_to tasks_path, notice: 'Task was successfully updated.'
+      flash[:notice] = "Task was successfully updated."
     else
       render :edit
     end
@@ -35,6 +37,7 @@ class TasksController < ApplicationController
   def destroy
     @task.destroy
     redirect_to tasks_path, notice: 'Task was successfully destroyed.'
+    flash[:notice] = "Task was successfully destroyed."
   end
 
   private
