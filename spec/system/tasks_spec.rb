6d295bcd1ef0aa17abd3506729e9a2db68ccc0e1
井出晶 <badminton.1110@i.softbank.jp>
2022-11-09 18:59:46 +0900

    システムスペック

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -4,9 +4,10 @@ RSpec.describe 'タスク管理機能', type: :system do
   describe '登録機能' do
     context 'タスクを登録した場合' do
       it '登録したタスクが表示される' do
-        FactoryBot.create(:task)
+        FactoryBot.create(:task, title: '書類作成', content: '企画書を作成する。')
         visit tasks_path
         expect(page).to have_content '書類作成'
+        expect(page).to have_content '企画書を作成する。'
       end
     end
   end
@@ -14,9 +15,14 @@ RSpec.describe 'タスク管理機能', type: :system do
   describe '一覧表示機能' do
     context '一覧画面に遷移した場合' do
       it '登録済みのタスク一覧が表示される' do
-        FactoryBot.create(:task)
+        # テストで使用するためのタスクを登録
+        FactoryBot.create(:task, title: '書類作成', content: '企画書を作成する。')
+        # タスク一覧画面に遷移
         visit tasks_path
+        # visit（遷移）したpage（この場合、タスク一覧画面）に"書類作成"という文字列が、have_content（含まれていること）をexpect（確認・期待）する
         expect(page).to have_content '書類作成'
+        expect(page).to have_content '企画書を作成する。'
+        # expectの結果が「真」であれば成功、「偽」であれば失敗としてテスト結果が出力される
       end
     end
   end
@@ -24,9 +30,11 @@ RSpec.describe 'タスク管理機能', type: :system do
   describe '詳細表示機能' do
     context '任意のタスク詳細画面に遷移した場合' do
       it 'そのタスクの内容が表示される' do
-        FactoryBot.create(:task)
-        visit tasks_path
+        task = FactoryBot.create(:task, title: '書類作成', content: '企画書を作成する。')
+        visit task_path(task)
+        # click_link 'Show'
         expect(page).to have_content '書類作成'
+        expect(page).to have_content '企画書を作成する。'
       end
     end
   end
