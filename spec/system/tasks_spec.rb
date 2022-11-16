cf2a9518361081c70a93d0da651560bc437a3991
井出晶 <badminton.1110@i.softbank.jp>
2022-11-16 16:25:06 +0900

    task.rb修正

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -1,5 +1,4 @@
 require 'rails_helper'
-
 RSpec.describe 'タスク管理機能', type: :system do
   describe '登録機能' do
     context 'タスクを登録した場合' do
@@ -17,16 +16,15 @@ RSpec.describe 'タスク管理機能', type: :system do
   end
 
   describe '一覧表示機能' do
-  # let!を使ってテストデータを変数として定義することで、複数のテストでテストデータを共有できる
-  let!(:first_task) { FactoryBot.create(:first_task, title: 'task_title1') }
-  let!(:second_task) { FactoryBot.create(:second_task, title: 'task_title2') }
-  let!(:third_task) { FactoryBot.create(:third_task, title: 'task_title3') }
-  let(:forth_task) { FactoryBot.create(:task) }
-  # 「一覧画面に遷移した場合」や「タスクが作成日時の降順に並んでいる場合」など、contextが実行されるタイミングで、before内のコードが実行される
-  before do
-    visit tasks_path
-  end
-
+    # let!を使ってテストデータを変数として定義することで、複数のテストでテストデータを共有できる
+    let!(:first_task) { FactoryBot.create(:first_task, title: 'task_title1') }
+    let!(:second_task) { FactoryBot.create(:second_task, title: 'task_title2') }
+    let!(:third_task) { FactoryBot.create(:third_task, title: 'task_title3') }
+    let(:forth_task) { FactoryBot.create(:task) }
+    # 「一覧画面に遷移した場合」や「タスクが作成日時の降順に並んでいる場合」など、contextが実行されるタイミングで、before内のコードが実行される
+    before do
+      visit tasks_path
+    end
 
     context '一覧画面に遷移した場合' do
       it '作成済みのタスク一覧が作成日時の降順で表示される' do
@@ -36,7 +34,6 @@ RSpec.describe 'タスク管理機能', type: :system do
         expect(task_list[3].text).to have_content 'task_title3'
       end
     end
-
     context '新たにタスクを作成した場合' do
       it '新しいタスクが一番上に表示される' do
         task = forth_task.title
@@ -46,7 +43,6 @@ RSpec.describe 'タスク管理機能', type: :system do
       end
     end
   end
-
   describe '詳細表示機能' do
     context '任意のタスク詳細画面に遷移した場合' do
       it 'そのタスクの内容が表示される' do
