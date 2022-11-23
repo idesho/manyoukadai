b540125bc71d7ba846ab85810097e4c1777be131
井出晶 <badminton.1110@i.softbank.jp>
2022-11-23 22:05:34 +0900

    途中経過

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,66 +5,75 @@ class TasksController < ApplicationController
   before_action :correct_user, only: [:show, :edit]
 
   def index
-    @tasks = current_user.tasks
+    @tasks = @current_user.tasks
+
     # 終了期限/優先度ソート機能
     if params[:sort_deadline_on]
       @tasks = @tasks.sort_deadline_on
     elsif params[:sort_priority]
       @tasks = @tasks.sort_priority
     end
-
     # 検索機能
     if params[:search].present?
       @tasks = @tasks
-      .search_status(params[:search][:status])
-      .search_title(params[:search][:title])
+        .search_status(params[:search][:status])
+        .search_title(params[:search][:title])
+        .search_label(params[:search][:label_id])
     end
+    # ページネーション
     @tasks = @tasks.page(params[:page]).default_order
-  end 
-
+    # 下記はmodelへ記載
+    # if params[:search].present?
+    #   if params[:search][:status].present? && params[:search][:title].present?
+    #     @tasks = @tasks.search_status(params[:search][:status]).search_title(params[:search][:title])
+    #   elsif params[:search][:status].present?
+    #     @tasks = @tasks.search_status(params[:search][:status])
+    #   elsif params[:search][:title].present?
+    #     @tasks = @tasks.search_title(params[:search][:title])
+    #   end 
+    # end
+  end
   def new
     @task = Task.new
   end
-  
   def create
     @task = Task.new(task_params)
     @task.user_id = current_user.id
     if @task.save
+      # @label = LabelTask.new(label_id: params[:task][:label_ids].first, task_id: @task.id)
+      @task.labels.push(Label.find(params[:task][:label_ids]))
       redirect_to tasks_path, notice: Task.human_attribute_name(:task_created)
     else
       render :new
     end
   end
-
   def show
   end
-
   def edit
   end
-
   def update
     if @task.update(task_params)
+      if params[:task][:label_ids].present?
+        @task.labels.push(Label.find(params[:task][:label_ids]))
+      else
+        @task.labels.destroy
+      end
       redirect_to tasks_path, notice: Task.human_attribute_name(:task_updated)
     else
       render :edit
     end
   end
-
   def destroy
     @task.destroy
     redirect_to tasks_path, notice: Task.human_attribute_name(:task_destroyed)
   end
-  
   private
-  
     def set_task
       @task = Task.find(params[:id])
     end
-
     def task_params
-      params.require(:task).permit(:title, :content, :deadline_on, :priority, :status)
+      params.require(:task).permit(:title, :content, :deadline_on, :priority, :status, :label_ids)
     end
-
     def correct_user
       user_id = Task.find(params[:id]).user_id
       redirect_to tasks_path, notice: User.human_attribute_name(:correct_user)  unless current_user?(user_id)
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,12 +1,17 @@
 class Task < ApplicationRecord
   belongs_to :user
+  has_many :label_tasks, dependent: :destroy
+  has_many :labels, through: :label_tasks
+
   validates :title, presence: true
   validates :content, presence: true
   validates :deadline_on, presence: true
   validates :priority, presence: true
   validates :status, presence: true
+  # enum優先度・テータス用
   enum priority: { 低: 0, 中: 1, 高: 2 }
   enum status: { 未着手: 0, 着手中: 1, 完了: 2 }
+  # scope検索・ソート機能用
   scope :default_order, -> { order("created_at DESC") }
   scope :sort_deadline_on, -> { order("deadline_on") }
   scope :sort_priority, -> { order("priority DESC") }
@@ -16,4 +21,7 @@ class Task < ApplicationRecord
   scope :search_title, ->(title) {
     return if title.blank?
     where('title LIKE ?',"%#{title}%") }
+  scope :search_label, ->(label) {
+    return if label.blank?
+    where(id: LabelTask.where(label_id: label).pluck(:task_id))}
 end
