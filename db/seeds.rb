223b827b16585dbba2d732709e7454ced870b32c
井出晶 <badminton.1110@i.softbank.jp>
2022-11-24 12:35:59 +0900

    spec修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,2 @@
 User.create!(name: "adminsさん", email: "adminadmino@piyopiyo.com", password: "123456", password_confirmation: "123456", admin: true)
 User.create!(name: "normalさん", email: "normalnormal@piyopiyo.com", password: "123456", password_confirmation: "123456", admin: false)
-
-# 50.times do
-#   Task.create!(title: "first_task", content: "テスト", deadline_on: "2025/02/18", priority: 1, status: 0, user_id: User.first.id)
-# end
-
-# 50.times do
-#   Task.create!(title: "second_task", content: "テスト", deadline_on: "2025/02/17", priority: 2, status: 1, user_id: User.second.id)
-# end
